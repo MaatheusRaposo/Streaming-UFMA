eba132016e1a6d7fbed1d9be880f069c51b139db
MaatheusRaposo <matheus.compta@gmail.com>
2026-09-15 10:26:30 -0300

    feat: anotacoes de associacao para ProgressoVisualizacao

--- src/main/java/com/lab_prog/streaming/model/entities/Midia.java
+++ src/main/java/com/lab_prog/streaming/model/entities/Midia.java
@@ -10,6 +10,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
@@ -23,6 +27,16 @@ public class Midia {
     @Id 
     @GeneratedValue 
     private UUID midiaId;
+
+    @ManyToOne 
+    @MapsId 
+    @JoinColumn(name="user_id")
+    private Usuario user;
+
+    @ManyToOne 
+    @MapsId 
+    @JoinColumn(name="ep_id")
+    private Episodio ep;
 
     private String titulo;
     private String sinopse;
